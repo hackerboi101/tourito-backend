e4d56a3fb50efac63b157ede526a64c022e95b57
hackerboi101 <asif.iq.024@gmail.com>
2025-10-18 23:33:16 +0600

    Starting of Something Great
    This is the commit where something great starts

---
+++ src/main/java/com/division76/tourito/TouritoApplication.java
@@ -0,0 +1,12 @@
+package com.division76.tourito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouritoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouritoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/division76/tourito/TouritoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.division76.tourito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouritoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
